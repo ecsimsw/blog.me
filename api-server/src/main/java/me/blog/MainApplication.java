d21c5b803a37d0361d39f4e78ab822eab35b3e8f
ecsimsw <ecsimsw@gmail.com>
2024-02-28 02:21:29 +0900

    tistory crawler

--- api-server/src/main/java/me/blog/MainApplication.java
+++ api-server/src/main/java/me/blog/MainApplication.java
@@ -1,6 +1,6 @@
 package me.blog;
 
-import me.blog.crawler.TistoryArticle;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import shutdown.EnableShutDown;
 
@@ -9,11 +9,7 @@ import shutdown.EnableShutDown;
 public class MainApplication {
 
     public static void main(String[] args) {
-//        SpringApplication app = new SpringApplication(MainApplication.class);
-//        app.run(args);
-
-        TistoryArticle
-            .crawl("https://ecsimsw.tistory.com/entry/1000%EB%A7%8C%EA%B0%9C-%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%A1%9C-%EC%BF%BC%EB%A6%AC-%EC%84%B1%EB%8A%A5-%ED%99%95%EC%9D%B8%ED%95%98%EA%B8%B0")
-            .download("./storage-articles/test3.html");
+        var app = new SpringApplication(MainApplication.class);
+        app.run(args);
     }
 }
--- api-server/src/main/java/me/blog/crawler/TistoryArticle.java
+++ api-server/src/main/java/me/blog/crawler/TistoryArticle.java
@@ -5,30 +5,31 @@ import java.io.File;
 import java.io.IOException;
 import org.jsoup.Jsoup;
 
-public class TistoryArticle {
+public record TistoryArticle(String url, String articleHtml) {
 
     private static final String ARTICLE_TITLE_ELEMENT_CLASS_NAME = "tit_post";
     private static final String ARTICLE_BODY_ELEMENT_CLASS_NAME = "tt_article_useless_p_margin";
     private static final String ARTICLE_FORMAT =
-        "<meta charset=\"utf-8\">\n"
-            + "<html lang=\"ko\">\n"
-            + "<link rel=\"stylesheet\" type=\"text/css\" href=\"../style.css\"/>\n"
-            + "<body>\n"
-            + "<div class=\"wrap-right\">\n"
-            + "    <main class=\"main \">\n"
-            + "        <div class=\"area-main\">\n"
-            + "             <div class=\"article-header\">\n"
-            + "                 %s"
-            + "             </div>\n"
-            + "             <div class=\"article-view\">\n"
-            + "                 %s"
-            + "             </div>\n"
-            + "        </div>\n"
-            + "    </main>\n"
-            + "</div>\n"
-            + "</body>\n"
-            + "</html>"
-            + ">";
+    """
+        <meta charset="utf-8">
+        <html lang="ko">
+        <link rel="stylesheet" type="text/css" href="../style.css"/>
+        <body>
+        <div class="wrap-right">
+            <div class="main ">
+                <div class="area-main">
+                     <div class="article-header">
+                         %s            
+                     </div>
+                     <div class="article-view">
+                         %s             
+                     </div>
+                </div>
+            </main>
+        </div>
+        </body>
+        </html>>
+    """;
 
     public static TistoryArticle crawl(String url) {
         return crawl(url, ARTICLE_TITLE_ELEMENT_CLASS_NAME, ARTICLE_BODY_ELEMENT_CLASS_NAME);
@@ -47,28 +48,13 @@ public class TistoryArticle {
         }
     }
 
-    private final String url;
-    private final String articleHtml;
-
-    public TistoryArticle(String url, String articleHtml) {
-        this.url = url;
-        this.articleHtml = articleHtml;
-    }
-
     public void download(String filePath) {
         try {
             File file = new File(filePath);
             Files.write(articleHtml.getBytes(), file);
+            System.out.println("Download complete : " + filePath);
         } catch (IOException e) {
             throw new IllegalArgumentException("failed to write as file", e);
         }
-    }
-
-    public String url() {
-        return url;
-    }
-
-    public String articleHtml() {
-        return articleHtml;
     }
 }
