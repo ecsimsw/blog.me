143639fb3d39429ded81b9e9a98bf878c3813556
ecsimsw <ecsimsw@gmail.com>
2024-02-27 20:20:57 +0900

    update style

--- api-server/src/main/java/me/blog/MainApplication.java
+++ api-server/src/main/java/me/blog/MainApplication.java
@@ -13,7 +13,7 @@ public class MainApplication {
 //        app.run(args);
 
         TistoryArticle
-            .crawl("https://swmobenz.tistory.com/36")
-            .download("./storage-articles/test22.html");
+            .crawl("https://ecsimsw.tistory.com/entry/1000%EB%A7%8C%EA%B0%9C-%EB%8D%B0%EC%9D%B4%ED%84%B0%EB%A1%9C-%EC%BF%BC%EB%A6%AC-%EC%84%B1%EB%8A%A5-%ED%99%95%EC%9D%B8%ED%95%98%EA%B8%B0")
+            .download("./storage-articles/test3.html");
     }
 }
